8a52fdbc4ec175162a3c634f9cfc5afeebea3a0e
mri rashid <mdislam0996@gmail.com>
2023-12-09 13:05:13 +0800

    first commit

---
+++ comment.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void){
+//Program comments are nonexecuting statements you add to a program for the purpose of documentation.//;
+/*Program comments are nonexecuting statements you add to a program for the purpose of documentation.*/;
+printf("Program comments are nonexecuting statements you add to a program for the purpose of documentation.\n");
+
+return 0;
+}
